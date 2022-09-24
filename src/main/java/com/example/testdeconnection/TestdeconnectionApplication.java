2232d7392f8b6aac5d1605adb28bff527592e2d1
Momo-azangue <azanguewill@gmail.com>
2022-09-24 11:06:18 +0100

    first springboot application

---
+++ src/main/java/com/example/testdeconnection/TestdeconnectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.testdeconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestdeconnectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestdeconnectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testdeconnection/TestdeconnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testdeconnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestdeconnectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
